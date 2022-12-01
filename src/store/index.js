a720f88fa863939806e30f15f30f0ea7e76233a6
Mike <57944493+nux7jr@users.noreply.github.com>
2022-12-01 22:05:23 +0700

    01.12.2022

--- src/store/index.js
+++ src/store/index.js
@@ -6,35 +6,31 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   actions: {
     async getProducts(context) {
-      // for me
-    //   const products = await axios.post('http://api.tiksan.ru/api/products', {
-    //       headers: {
-    //         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    //         'Connection': 'keep-alive',
-    //         'Accept': 'application/json, */*; q=0.01'
-    //       },
-    //       body: {
-    //         action: "all",
-    //       }
-    // })
+      const params = new URLSearchParams();
+      const headers = new Headers();
+      headers.append("Content-Type", "application/x-www-form-urlencoded");
+      headers.append(
+        "Accept",
+        "application/json;text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+      );
+      params.set("action", "all");
+      const products = await fetch("https://api.tiksan.ru/api/products", {
+        method: "POST",
+        headers: headers,
+        body: params,
+        mode: "cors",
+      });
 
-    const res = await axios.post('http://api.tiksan.ru/api/products', { action: 'all' }, {
-      headers: {
-        // 'application/json' is the modern content-type for JSON, but some
-        // older servers may use 'text/json'.
-        // See: http://bit.ly/text-json
-        'content-type': 'text/json'
-      }
-    });
-    res.data.headers['Content-Type'];
-          console.log(res)
-          // context.commit("updateProducts", products);
-          // for prod 
-          // поставить prod true
-          // const products = await axios.get("https://tiksan-record.ru/db.json");
-          // context.commit("updateProducts", products.data.products);
-        },
-      },
+      let res = await products; // тут ждать промис!
+      console.log("PROD:", res);
+
+      // context.commit("updateProducts", products);
+
+      // поставить prod true
+      // const products = await axios.get("https://tiksan-record.ru/db.json");
+      // context.commit("updateProducts", products.data.products);
+    },
+  },
   mutations: {
     updateProducts(state, products) {
       state.products = products;
@@ -45,11 +41,10 @@ export default new Vuex.Store({
       } else {
         state.cart = products;
       }
-
     },
     fullReprimandCart(state) {
       if (Object.keys(state.cart).length == 0) {
-        console.log('пуст');
+        console.log("пуст");
       } else {
         state.cart = {};
       }
